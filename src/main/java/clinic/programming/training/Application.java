6ffacd1ae4cb4ec8c94455af1cffe90824d8e249
Matthias Riesch <mriesch@gmail.com>
2017-10-15 12:17:24 +0200

    now with StringUtils

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,9 +1,18 @@
 package clinic.programming.training;
 
+
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
     
+	public int countWords(String words) {
+	  String[] separateWords = StringUtils.split(words,' ');
+	  return (separateWords == null) ? 0 : separateWords.length;
+	}
+	
     public Application() {
         System.out.println ("Inside Application");
+		
     }
 
     // method main(): ALWAYS the APPLICATION entry point
